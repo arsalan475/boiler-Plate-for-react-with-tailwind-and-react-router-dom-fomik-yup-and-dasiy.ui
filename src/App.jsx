11943d6e3b09aae688ce32d80ca124946708be4d
arsalan aslam <arsalanaslam475@gmail.com>
2024-01-21 12:12:29 +0500

    react boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+
+function App() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+    <div className="hero-content text-center">
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold">React Boiler Plate</h1>
+        <p className="py-6">That's template for react vite app with react-router-dom fomik yup and tailwind and with tailwind plugin daisy.ui</p>
+        <button className="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
